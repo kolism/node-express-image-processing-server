2a86d322bb6bcf09bdab0b9240d9d6fe0ff2b98e
kolism <14796164+kolism@users.noreply.github.com>
2022-10-07 04:08:30 -0400

    Module4

--- api/src/router.js
+++ api/src/router.js
@@ -1,37 +1,42 @@
 const Router = require('express').Router
-const m = require('gm')
+
 const multer = require('multer')
-const { response, request } = require('../app')
+
+const path = require('path')
+const photoPath = path.resolve(__dirname,'../../client/photo-viewer.html')
+const router = Router()
+
+
+
+function filename(request,file,callback){
+    callback(null, file.originalname)
+}
+
 const storage = multer.diskStorage({
-    destination:'api/uploads',
+    destination:'api/uploads/',
     filename:filename
 })
 
-const router = Router()
-
-router.post('/upload',upload.single('photo'),(req,res)=>{
-    if(req.hasOwnProperty(fileValidationError)){
-
-        return response.status(400).json({error:req.fileValidationError})
-    }else{
-        return response.status(201).json({success:true})
-    }
-})
-
-function filename(request,file,callback){
-    callback(null, file.originalName)
-}
 
 function fileFilter(request,file,callback){
     if(file.mimetype !== 'image/png'){
-        request.fileValidationError("Wrong file type")
-        callback(null, false, new Error("Wrong file type"))
+        request.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type'));
     }else{
-        callback(null,true)
+        callback(null,true);
     }
 }
 
-const upload = multer({fileFilter:fileFilter,storage:storage})
+const upload = multer({fileFilter,storage})
+router.post('/upload',upload.single('photo'),(request,response)=>{
+    if(request.fileValidationError)
+        return response.status(400).json({error:request.fileValidationError})
+    
+    return response.status(201).json({success:true})
+})
+router.get('/photo-viewer',(request,response)=>{
+    response.sendFile(photoPath);
+})
 
 
 module.exports = router
